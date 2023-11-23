2516d94d50d28219fa03376900f96e24fbd173c4
huqianjin <38059167+huqianjin@users.noreply.github.com>
2023-11-23 20:28:25 +0800

    test

--- tampermonkey/test2.js
+++ tampermonkey/test2.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         test
+// @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @match        https://vscode.dev/github/huqianjin/tampermonkey
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=vscode.dev
 // @grant        none
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.js
 // ==/UserScript==
 
 (function() {
